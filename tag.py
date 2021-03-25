0c76fb039450c657733e2c51f3f09975037a9bca
robinelting <r.elting.1@student.rug.nl>
2021-03-25 12:10:55 +0100

    Changed s tags in tag.py

--- tag.py
+++ tag.py
@@ -3,9 +3,9 @@ import re
 import pysrt
 
 def tokenizer(sentence):
-    sentence = re.sub(r'[^\w\s]', " ", sentence)
-    sentence = sentence.lower()
-    return sentence
+	sentence = re.sub(r'[^\w\s]', " ", sentence)
+	sentence = sentence.lower()
+	return sentence
 
 def tagger(content):
 	tagged_sentences = []
@@ -16,57 +16,52 @@ def tagger(content):
 			tagged_sentences.append('M|\t' + line)
 	return tagged_sentences
 
-def s_tagger(text):
-    tagged_sentences = []
-    for line in text:
-        for mark in ['EXT.', 'INT.']:
-            if mark in line:
-                tagged_sentences.append('S|\t'+ line)
-    return tagged_sentences
 
 def m_tagger(text):
-    tagged_sentences = []
-    for line in text:
-        if 'CUT TO:' in line:
-        	tagged_sentences.append('M|\t'+ line)
-        elif 'ON THE SCREEN' in line:
-        	tagged_sentences.append('M|\t'+ line)
-        elif 'IN THE CLOSET' in line:
-        	tagged_sentences.append('M|\t'+ line)
-    return tagged_sentences
+	tagged_sentences = []
+	for line in text:
+		if 'CUT TO:' in line:
+			tagged_sentences.append('M|\t'+ line)
+		elif 'ON THE SCREEN' in line:
+			tagged_sentences.append('M|\t'+ line)
+		elif 'IN THE CLOSET' in line:
+			tagged_sentences.append('M|\t'+ line)
+	return tagged_sentences
 
 def c_tagger(text):
-    tagged_sentences = []
-    for item in text:
-        tagged_sentences.append(re.findall(r'[A-Z]+$', item))
-    return tagged_sentences
+	tagged_sentences = []
+	for item in text:
+		tagged_sentences.append(re.findall(r'[A-Z]+$', item))
+	return tagged_sentences
 
 
 def main():
-    with open(sys.argv[1], 'rt', encoding='utf8') as f:
-        file_content = f.read()
+	with open(sys.argv[1], 'rt', encoding='utf8') as f:
+		file_content = f.read()
 
-    file_content = file_content.split('\n\n')
-    preprocessed_subs = []
-    for line in file_content:
-        line = re.sub(' +',' ',line)
-        line = re.sub('\n', ' ', line)
-        if line != '':
+	file_content = file_content.split('\n\n')
+	preprocessed_subs = []
+	for line in file_content:
+		line = re.sub(' +',' ',line)
+		line = re.sub('\n', ' ', line)
+		line = re.sub('  ', '', line)
+		line = re.sub(r'^ ', '', line)
+		if line != '':
 #            line = tokenizer(line)
-            preprocessed_subs.append(line)
-    
-    print(preprocessed_subs)
-    
-    s_tagged = s_tagger(preprocessed_subs)
-    #print(s_tagged)
-    m_tagged = m_tagger(preprocessed_subs)
-    #print(m_tagged)
-    c_tagged = c_tagger(preprocessed_subs)
-    #print(c_tagged)
-    tagged = tagger(preprocessed_subs)
-    #print(tagged)
-   
+			preprocessed_subs.append(line)
+	
+	#print(preprocessed_subs)
+
+	s_tagged = []
+	for line in preprocessed_subs:
+		res1 = re.search(r'^INT', line)
+		res2 = re.search(r'^EXT', line)
+		if res1:
+			s_tagged.append('S|\t' + res1.string)
+		elif res2:
+			s_tagged.append('S|\t' + res2.string)
+	print(s_tagged)
 
 
 if __name__ == "__main__":
-    main()
+	main()
