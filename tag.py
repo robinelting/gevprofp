30590f6a9e0ffb9f50a749bbd5b27f4546048e76
robinelting <60693419+robinelting@users.noreply.github.com>
2021-04-14 14:57:54 +0200

    Updated function dic_tagger

--- tag.py
+++ tag.py
@@ -2,15 +2,40 @@ import sys
 import re
 import pysrt
 import nltk
-from linecache import getline
-from itertools import islice
 import csv
-import json
 
 def tokenizer(sentence):
 	sentence = re.sub(r'[^\w\s]', " ", sentence)
 	sentence = sentence.lower()
 	return sentence
+
+
+def tagger(content):
+	tagged_sentences = []
+	dic_tag = dict()
+	for line in content:
+		scene = re.search(r'INT\.|EXT\.', line)
+		meta1 = re.search(r'^ {5}ON THE|^ {5}IN THE|CUT TO:|THE END|FINAL SHOOTING SCRIPT', line)
+		meta2 = re.search(r'[A-Z]\:|  \([A-Za-z0-9 ]+\)', line)
+		character = re.search(r' {26}[A-Z]{3,}', line)
+		descr = re.search(r'^ {5}[A-Za-z]|^ {10}[A-Za-z]', line)
+		dialogue = re.search(r' {16}[A-Za-z \.\'?!,]{10,}', line)
+		if scene:
+			tagged_sentences.append('S|\t' + scene.string)
+		elif meta1:
+			tagged_sentences.append('M|\t' + meta1.string)
+		elif meta2:
+			tagged_sentences.append('M|\t' + meta2.string)
+		elif character:
+			tagged_sentences.append('C|\t\t' + character.string)
+		elif descr:
+			tagged_sentences.append('N|\t' + descr.string)
+		elif dialogue:
+			tagged_sentences.append('D|\t' + dialogue.string)
+		else:
+			tagged_sentences.append(' |\t' + line)
+	return tagged_sentences
+
 
 
 def dic_tagger(content):
@@ -23,30 +48,31 @@ def dic_tagger(content):
 		meta1 = re.search(r'^ {5}ON THE|^ {5}IN THE|CUT TO:|THE END|FINAL SHOOTING SCRIPT', line)
 		meta2 = re.search(r'[A-Z]\:|  \([A-Za-z0-9 ]+\)', line)
 		character = re.search(r' {26}[A-Z]{3,}', line)
-		descr = re.search(r'^ {5}[A-Za-z]', line)
+		descr = re.search(r'^ {5}[A-Za-z]|^ {10}[A-Za-z]', line)
 		dialogue = re.search(r' {16}[A-Za-z ]{10,}', line)
 		if scene:
-			dic_tag[scene.string] = 'S'
+			dic_tag[content.index(line)] = tuple(['S', scene.string])
 		elif meta1:
-			dic_tag[meta1.string] = 'M'
+			dic_tag[content.index(line)] = tuple(['M', meta1.string])
 		elif meta2:
-			dic_tag[meta2.string] = 'M'
+			dic_tag[content.index(line)] = tuple(['M', meta2.string])
 		elif character:
-			dic_tag[character.string] = 'C'
+			dic_tag[content.index(line)] = tuple(['C', character.string])
 		elif descr:
-			dic_tag[descr.string] = 'N'
+			dic_tag[content.index(line)] = tuple(['N', descr.string])
 		elif dialogue:
-			dic_tag[dialogue.string] = 'D'
+			dic_tag[content.index(line)] = tuple(['D', dialogue.string])
 		else:
-			dic_tag[line] = ' '
+			dic_tag[content.index(line)] = tuple([' ', line])
 	
 	new_dic = dict()
 	for k, v in dic_tag.items():
-		k2 = re.sub(r' {3,}', '', k)
-		if k2 not in new_dic:
-			new_dic[k2] = v
+		for value in v:
+			value = re.sub(r' {3,}', '', value)
+			if v not in new_dic:
+				new_dic[k] = v
 
-	return new_dic
+	return dic_tag
 
 
 def main():
@@ -56,6 +82,10 @@ def main():
 	file_content = file_content.split('\n')
 
 	tag_dic = dic_tagger(file_content)
+	sents = tagger(file_content)
+
+	print(tag_dic)
+	#print('\n'.join(sents))
 
 	with open('output.csv', 'w') as output:
 		writer = csv.writer(output)
