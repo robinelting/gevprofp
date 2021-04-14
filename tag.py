596be90cb35dde246e467ada199992563888ec96
robinelting <60693419+robinelting@users.noreply.github.com>
2021-04-14 17:57:09 +0200

    Add files via upload

--- tag.py
+++ tag.py
@@ -3,6 +3,9 @@ import re
 import pysrt
 import nltk
 import csv
+import pandas
+from collections import defaultdict
+from char import subtitles_preprocessing
 
 def tokenizer(sentence):
 	sentence = re.sub(r'[^\w\s]', " ", sentence)
@@ -12,7 +15,6 @@ def tokenizer(sentence):
 
 def tagger(content):
 	tagged_sentences = []
-	dic_tag = dict()
 	for line in content:
 		scene = re.search(r'INT\.|EXT\.', line)
 		meta1 = re.search(r'^ {5}ON THE|^ {5}IN THE|CUT TO:|THE END|FINAL SHOOTING SCRIPT', line)
@@ -21,21 +23,20 @@ def tagger(content):
 		descr = re.search(r'^ {5}[A-Za-z]|^ {10}[A-Za-z]', line)
 		dialogue = re.search(r' {16}[A-Za-z \.\'?!,]{10,}', line)
 		if scene:
-			tagged_sentences.append('S|\t' + scene.string)
+			tagged_sentences.append(['S', scene.string])
 		elif meta1:
-			tagged_sentences.append('M|\t' + meta1.string)
+			tagged_sentences.append(['M', meta1.string])
 		elif meta2:
-			tagged_sentences.append('M|\t' + meta2.string)
+			tagged_sentences.append(['M', meta2.string])
 		elif character:
-			tagged_sentences.append('C|\t\t' + character.string)
+			tagged_sentences.append(['C', character.string])
 		elif descr:
-			tagged_sentences.append('N|\t' + descr.string)
+			tagged_sentences.append(['N', descr.string])
 		elif dialogue:
-			tagged_sentences.append('D|\t' + dialogue.string)
+			tagged_sentences.append(['D', dialogue.string])
 		else:
-			tagged_sentences.append(' |\t' + line)
+			tagged_sentences.append(['?', line])
 	return tagged_sentences
-
 
 
 def dic_tagger(content):
@@ -44,35 +45,22 @@ def dic_tagger(content):
 	"""
 	dic_tag = dict()
 	for line in content:
-		scene = re.search(r'INT\.|EXT\.', line)
+		scene = re.compile(r'INT\.|EXT\.')
 		meta1 = re.search(r'^ {5}ON THE|^ {5}IN THE|CUT TO:|THE END|FINAL SHOOTING SCRIPT', line)
 		meta2 = re.search(r'[A-Z]\:|  \([A-Za-z0-9 ]+\)', line)
 		character = re.search(r' {26}[A-Z]{3,}', line)
 		descr = re.search(r'^ {5}[A-Za-z]|^ {10}[A-Za-z]', line)
 		dialogue = re.search(r' {16}[A-Za-z ]{10,}', line)
-		if scene:
-			dic_tag[content.index(line)] = tuple(['S', scene.string])
-		elif meta1:
-			dic_tag[content.index(line)] = tuple(['M', meta1.string])
-		elif meta2:
-			dic_tag[content.index(line)] = tuple(['M', meta2.string])
-		elif character:
-			dic_tag[content.index(line)] = tuple(['C', character.string])
-		elif descr:
-			dic_tag[content.index(line)] = tuple(['N', descr.string])
-		elif dialogue:
-			dic_tag[content.index(line)] = tuple(['D', dialogue.string])
-		else:
-			dic_tag[content.index(line)] = tuple([' ', line])
-	
-	new_dic = dict()
-	for k, v in dic_tag.items():
-		for value in v:
-			value = re.sub(r' {3,}', '', value)
-			if v not in new_dic:
-				new_dic[k] = v
+		
+		scenes = re.findall(r'INT\.|EXT\.', line)
+		characters = re.findall(r' {26}[A-Z]{3,}', line)
+		if scenes:
+			dic_tag[content.index(line)] = line
+		elif characters:
+			dic_tag[content.index(line)] = line
 
 	return dic_tag
+
 
 
 def main():
@@ -83,15 +71,18 @@ def main():
 
 	tag_dic = dic_tagger(file_content)
 	sents = tagger(file_content)
+	subtitles, timestamps = subtitles_preprocessing()
+	
+	for item in sents:
+		#print(item[1])
+		for subtitle in subtitles:
+			if subtitle in item[1]:
+				item.append(subtitle)
 
-	print(tag_dic)
-	#print('\n'.join(sents))
 
-	with open('output.csv', 'w') as output:
-		writer = csv.writer(output)
-		for key, value in tag_dic.items():
-			writer.writerow([key, value])
-
+	df = pandas.DataFrame(sents)
+	
+	df.to_csv(r'./output.csv', index=False, header=True)
 
 if __name__ == "__main__":
 	main()
